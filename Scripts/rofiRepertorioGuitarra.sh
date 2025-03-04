de3eafbea4bfccb9a938ddb9cb702b51ece748fc
Juany <juanromeroruiz@gmail.com>
2025-03-04 16:36:06 -0300

    mejoro scripts y agrego el del repertorio

--- Scripts/rofiRepertorioGuitarra.sh
+++ Scripts/rofiRepertorioGuitarra.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DIR_NOTAS="/home/juani/Documentos/Notas Daily List/"
+DIR_NOTAS="/home/juani/Documentos/Repertorio Guitarra/"
 
 if [ -z "$DIR_NOTAS" ]; then
-    echo "No hay notas disponibles."
+    echo "No hay temas disponibles."
     exit 1
 fi
 
 # Opción para crear una nueva nota
-OPCIONES="  Crear nueva nota\n  Borrar nota\n$(
+OPCIONES="  Agregar un nuevo tema\n  Borrar tema\n$(
     find "$DIR_NOTAS" -type f -name "*.txt" -exec basename {} \;
 )"
 
@@ -16,28 +16,28 @@ OPCIONES="  Crear nueva nota\n  Borrar nota\n$(
 SELECCION=$(echo -e "$OPCIONES" | rofi -dmenu -p "󱞁 ")
 
 # Si la opción es "Crear nueva nota"
-if [[ "$SELECCION" == "  Crear nueva nota" ]]; then
-    NOMBRE_NOTA=$(rofi -dmenu -p "Nombre de la nueva nota: ")
+if [[ "$SELECCION" == "  Agregar un nuevo tema" ]]; then
+    NOMBRE_NOTA=$(rofi -dmenu -p "Nombre del tema: ")
     if [ -n "$NOMBRE_NOTA" ]; then
         RUTA_NOTA="$DIR_NOTAS/$NOMBRE_NOTA.txt"
         touch "$RUTA_NOTA"
-        notify-send "Notas" "Nota creada: \n<b>$NOMBRE_NOTA</b>"
+        notify-send "Repertorio" "Tema agregado: \n<b>$NOMBRE_NOTA</b>"
         #xfce4-terminal --hold -e "nvim '$RUTA_NOTA'"
         kitty -e nvim "$ABRIR_NOTA"
     fi
 # Si se selecciona "Borrar nota"
-elif [[ "$SELECCION" == "  Borrar nota" ]]; then
-    NOTA_A_BORRAR=$(find "$DIR_NOTAS" -type f -name "*.txt" -exec basename {} \; | rofi -dmenu -p "Seleccioná la nota a borrar: ")
+elif [[ "$SELECCION" == "  Borrar tema" ]]; then
+    NOTA_A_BORRAR=$(find "$DIR_NOTAS" -type f -name "*.txt" -exec basename {} \; | rofi -dmenu -p "Seleccioná el tema a borrar: ")
     if [ -n "$NOTA_A_BORRAR" ]; then
         RUTA_NOTA="$DIR_NOTAS/$NOTA_A_BORRAR"
         # Confirmación antes de borrar
         CONFIRMACION=$(echo -e "Sí\nNo" | rofi -dmenu -p "Borrar: $NOTA_A_BORRAR?")
         if [ "$CONFIRMACION" == "Sí" ]; then
             rm "$RUTA_NOTA"
-            notify-send "Notas" "Nota borrada: \n<b>$NOTA_A_BORRAR</b>"
+            notify-send "Repertorio" "Tema borrado: \n<b>$NOTA_A_BORRAR</b>"
             #rofi -e "Nota '$NOTA_A_BORRAR' eliminada."
         else 
-            notify-send "Notas" "Nota no borrada"
+            notify-send "Repertorio" "Tema no borrado"
         fi
     fi
 else
