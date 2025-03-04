1d3021eeb5a647b4e7b2262c8e355af5bb051344
Juany <juanromeroruiz@gmail.com>
2025-03-04 14:00:24 -0300

    mejoro el menu de musica con rofi y notificaciones

--- Scripts/rofiMusica.sh
+++ Scripts/rofiMusica.sh
@@ -6,9 +6,9 @@ artista=$(playerctl metadata artist)
 opcion=$(echo -e "$artista\n$titulo\n \n \n \n " | rofi -dmenu -i -selected-row 3 -p "󰝚 ")
 
 case "$opcion" in
-  " ") playerctl previous && notify-send "Playerctl" "Canción anterior";;
-  " ") playerctl play && notify-send "Playerctl" "Play";;
-  " ") playerctl pause && notify-send "Playerctl" "Pausa";;
-  " ") playerctl next && notify-send "Playerctl" "Siguiente canción";;
+  " ") playerctl previous && notify-send "Anterior" "Playerctl";;
+  " ") playerctl play && notify-send "Reproduciendo" "<b>$artista:</b> $titulo";;
+  " ") playerctl pause && notify-send "Pausa" "<b>$artista:</b> $titulo";;
+  " ") playerctl next && notify-send "Siguiente" "Playerctl";;
   *) exit 1;;
 esac
