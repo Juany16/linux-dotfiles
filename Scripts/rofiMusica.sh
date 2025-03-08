fd1b4c17b6c9e741a42b79d2b85785a664ae7218
Juany <juanromeroruiz@gmail.com>
2025-03-07 23:50:05 -0300

    mejoro el script de musica

--- Scripts/rofiMusica.sh
+++ Scripts/rofiMusica.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-titulo=$(playerctl metadata title)
 artista=$(playerctl metadata artist)
+titulo=$(playerctl metadata title)
+
+# Verificar si no hay canción sonando
+if [[ -z "$titulo" || -z "$artista" ]]; then
+    artista="Playerctl"
+    titulo="No hay reproducción"
+fi
 
 opcion=$(echo -e "$artista\n$titulo\n \n \n \n " | rofi -dmenu -i -selected-row 3 -p "󰝚 ")
 
 case "$opcion" in
-  #"$artista") brave-browser "https://search.brave.com/search?q=$(playerctl metadata -f '{{xesam:artist}}')";;
-  #"$titulo") brave-browser "https://search.brave.com/search?q=$(playerctl metadata -f '{{xesam:title}} {{xesam:artist}}') cifra club";;
-  #"$titulo") brave-browser "https://genius.com/search?q=$(playerctl metadata -f '{{xesam:title}} {{xesam:artist}}')";;
   " ") playerctl previous && notify-send "Anterior" "Playerctl";;
   " ") playerctl play && notify-send "Reproduciendo" "<b>$artista</b>\n$titulo";;
   " ") playerctl pause && notify-send "Pausa" "<b>$artista</b>\n$titulo";;
