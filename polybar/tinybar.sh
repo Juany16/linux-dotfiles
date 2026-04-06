6e98e78c7dde5bb521ca83237844a6c81fc1a5ea
Juany16 <juanromeroruiz16@gmail.com>
2026-04-06 20:12:43 -0300

    mejoro el Sys Tray con 2da polybar

--- polybar/tinybar.sh
+++ polybar/tinybar.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+if pgrep -f "polybar minibar" > /dev/null; then
+    exit 0
+fi
+
 echo "---" | tee -a /tmp/polybar2.log
 polybar minibar >> /tmp/polybar2.log 2>&1 & disown
 
 ~                                                                                                    
 ~  
-# sleep 0.2
-# polybar-msg -p $(pgrep -f "polybar minibar") cmd hide
